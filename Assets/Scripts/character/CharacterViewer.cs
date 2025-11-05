41b00508e2bd3c23e17a88b7e53ff1e655194bb9
yasuki2565 <ttc2550sg0018@edu.tech.ac.jp>
2025-11-06 01:55:56 +0900

    写真の差し替え　戻るの追加

--- Assets/Scripts/character/CharacterViewer.cs
+++ Assets/Scripts/character/CharacterViewer.cs
@@ -1,83 +1,55 @@
 using UnityEngine;
-
 using UnityEngine.UI;
-
 using TMPro;
-
 using UnityEngine.SceneManagement;
 
 public class CharacterViewer : MonoBehaviour
-
 {
-
     [System.Serializable]
-
     public class CharacterData
-
     {
-
         public Sprite image;
-
         public string name;
-
         [TextArea] public string description;
-
     }
 
     public CharacterData[] characters;
-
     public Image characterImage;
-
     public TMP_Text nameText;
-
     public TMP_Text descText;
 
     private int currentIndex = 0;
 
     void Start()
-
     {
-
         ShowCharacter(currentIndex);
-
     }
 
     public void NextCharacter()
-
     {
-
         currentIndex++;
-
-        if (currentIndex >= characters.Length) currentIndex = 0; // ループする
-
+        if (currentIndex >= characters.Length) currentIndex = 0; // ループ
         ShowCharacter(currentIndex);
-
     }
 
     public void PrevCharacter()
-
     {
-
         currentIndex--;
-
-        if (currentIndex < 0) currentIndex = characters.Length - 1;
-
+        if (currentIndex < 0) currentIndex = characters.Length - 1; // ループ
         ShowCharacter(currentIndex);
-
     }
 
     void ShowCharacter(int index)
-
     {
-
         CharacterData c = characters[index];
-
         characterImage.sprite = c.image;
-
         nameText.text = c.name;
-
         descText.text = c.description;
-
     }
 
+    
+    public void ReturnToTitle()
+    {
+        SceneManager.LoadScene("title");
+    }
 }
